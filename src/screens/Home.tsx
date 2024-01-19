1f7da2b1231035a89e6529e5f8e95877f5366953
emiequine <emi.roberti@equineregister.co.uk>
2024-01-19 06:39:57 +0000

    created todo List to be scrollable and Touchable Opacity

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,23 +1,52 @@
 import React, { FC } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, TouchableOpacity, Alert } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { MainStackParamList } from '../types/navigation';
+import { useQuery } from '@tanstack/react-query';
+import { ScrollView } from 'react-native-gesture-handler';
+import { Todo } from '../models/models';
+const endpoint = 'https://jsonplaceholder.typicode.com/todos';
 
 type HomeProps = {
   navigation: StackNavigationProp<MainStackParamList, 'Home'>;
 };
 const Home: FC<HomeProps> = ({ navigation }) => {
+  const {data:todos, isSuccess, isError, isPending} = useQuery({
+    queryKey:['todos'],
+    queryFn:()=> fetch(endpoint).then((res)=> res.json())
+  })
+
+  const interateToDos = () => {
+    return todos.map((t:Todo)=>(
+          <TouchableOpacity onPress={()=>navigation.navigate('Details', {todo:t})}>
+            <View style={styles.item}>
+              <Text style={styles.todoFont}>{t.title}</Text>
+            </View>
+        </TouchableOpacity>)
+      )
+  }
+
   return (
     <View style={styles.container}>
       <Text>Welcome Home</Text>
       <Button
         title="Home"
-        onPress={() => navigation.navigate('Details', { title: 'Emi code' })}
+        onPress={() => navigation.navigate('Details', { todo: {
+          userId:1,
+          id:2,
+          title:'RN Development',
+          completed:true
+          }}
+        )}
       />
-            <Button
+      <Button
         title="Settings"
         onPress={() => navigation.navigate('Settings')}
       />
+      {isSuccess ? <ScrollView 
+        style={styles.scrollableArea}>
+          {interateToDos()}
+        </ScrollView> : null}
     </View>
   );
 };
@@ -31,4 +60,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  item:{
+    borderBottomWidth:1,
+    borderColor:'gray',
+    marginTop:8,
+    marginBottom:8,
+    backgroundColor:'white',
+  },
+  scrollableArea:{
+    flex:1,
+    marginTop:16,
+    marginBottom:16
+  },
+  todoFont:{
+    fontSize:18,
+  }
 });
